144e1832624e4fcb1e0fb10f53e3f45225615ff7
Nester <vnesteriv@gmail.com>
2017-09-30 21:16:14 +0300

    Добавленно возможность регистрации

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
     category = models.CharField(max_length=200)
 
--- core/views.py
+++ core/views.py
@@ -3,10 +3,34 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 from core.models import Category, Subcategory, Product
 
 
 # Create your views here.
+@login_required
+def home(request):
+    return redirect('/menu/')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            #user = authenticate(username=username, password=raw_password)
+            #login(request, user)
+            return redirect('/login/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+
 def show_menu (request):
     products = Product.objects.order_by('product_category', 'product_subcategory')
     categorys = Category.objects.all()
